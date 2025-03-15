499f0186befcb339c056d664e1dae61d84c410e3
Onirau <agarioiopcmac@gmail.com>
2025-03-15 23:34:43 +0000

    wrapper update

--- wrapperFullscreen.js
+++ wrapperFullscreen.js
@@ -1,9 +1,12 @@
 function addScript(src) {
+    // Append a timestamp to the URL to avoid caching
+    let url = src + "?t=" + new Date().getTime();
+
     let script = document.createElement("script");
-    script.src = src;
+    script.src = url;
     script.defer = true;
     script.type = "text/javascript";
     document.getElementsByTagName("head")[0].appendChild(script);
 }
 
-addScript("https://raw.githubusercontent.com/Onirau/Spicetify-FAD/refs/heads/main/dist/test-app.js");
+addScript("https://onirau.github.io/FullscreenDisplay.js");
